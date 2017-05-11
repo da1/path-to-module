a379d025e99bd3d581a73e7aa3d7af72a61760fd
da1 <dyamada1988@gmail.com>
2017-05-11 13:14:41 +0900

    update

--- spec/path-to-module-view-spec.coffee
+++
@@ -1,6 +0,0 @@
-PathToModuleView = require '../lib/path-to-module-view'
-{WorkspaceView} = require 'atom'
-
-describe "PathToModuleView", ->
-  it "has one valid test", ->
-    expect("life").toBe "easy"
---
+++ spec/path-to-module-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import PathToModuleView from '../lib/path-to-module-view';
+
+describe('PathToModuleView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
